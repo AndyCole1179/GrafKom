c58c9e3d3de722acfbace01ccd3c085d069204c2
AndyCole1179 <134572850+AndyCole1179@users.noreply.github.com>
2024-02-10 17:15:47 +0800

    Update Kaca.py

--- Kaca.py
+++ Kaca.py
@@ -18,6 +18,14 @@ class Cermin:
             x = self.Center[0] + (radius*math.cos(theta-float(math.pi)))
             y = self.Center[1] + (radius*math.sin(theta-float(math.pi)))
             DrawC.gambar(math.ceil(x),math.ceil(y),self.Warna)
+            
+    def gambargaris (self,radius):
+        self.Radius = radius
+        self.perimeter = (400,300)
+        self.Center = (self.perimeter[0]-radius,self.perimeter[1])
+        self.focus = (self.perimeter[0]-(radius/2),self.perimeter[1])
+        DrawC.dda((400,0),(400,600),(255,0,0))
+
 
     def gambarfocus(self):
         DrawC.dda(self.focus,(self.focus[0],self.focus[1]-30),(255,0,0))
