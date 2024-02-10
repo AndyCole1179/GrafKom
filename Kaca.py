75bf1e525293f9b50817bfb033d428d615470267
AndyCole1179 <134572850+AndyCole1179@users.noreply.github.com>
2024-02-10 08:55:18 +0800

    Update and rename Settings.py to Kaca.py

---
+++ Kaca.py
@@ -0,0 +1,23 @@
+from Draw import DrawC
+import math
+class Cermin:
+    def __init__(self):
+        self.Radius= None
+        self.Center= None
+        self.perimeter = None
+        self.Warna = (125,0,0)
+        self.focus = None
+
+    def gambar (self,radius,perimeter):
+        self.Radius = radius
+        self.perimeter = perimeter
+        self.Center = (perimeter[0]-radius,perimeter[1])
+        self.focus = (perimeter[0]-(radius/2),perimeter[1])
+        for i in range (0, int(math.pi/0.01)):
+            theta = 2.4+i *0.005
+            x = self.Center[0] + (radius*math.cos(theta-float(math.pi)))
+            y = self.Center[1] + (radius*math.sin(theta-float(math.pi)))
+            DrawC.gambar(math.ceil(x),math.ceil(y),self.Warna)
+
+    def gambarfocus(self):
+        DrawC.dda(self.focus,(self.focus[0],self.focus[1]-30),(255,0,0))
