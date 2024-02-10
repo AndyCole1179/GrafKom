f3008a50b000049e23ce7c59865704927276389b
AndyCole1179 <134572850+AndyCole1179@users.noreply.github.com>
2024-02-10 09:00:23 +0800

    Update garis_cahaya.py
    change from manual imput warna to make variable global input for warna

--- garis_cahaya.py
+++ garis_cahaya.py
@@ -8,6 +8,7 @@ class garis:
         self.benda = bayangan.benda
         self.KoorCerminAtas=None
         self.KoorCerminBawah = None
+        self.warna = (255,255,0)
 
     def kalkulasi_garistemu(self,y,r):
         x = math.ceil(math.sqrt((r**2)-(y**2)))
@@ -19,7 +20,7 @@ class garis:
         x = self.kalkulasi_garistemu(y,r)
         print(x)
         self.KoorCerminAtas = (self.cermin.Center[0]+x,self.benda.kooratas[1])
-        DrawC.dda(self.benda.kooratas,self.KoorCerminAtas,(255,255,0))
+        DrawC.dda(self.benda.kooratas,self.KoorCerminAtas,self.warna)
         self.gic()
     
     def gic(self):
@@ -28,10 +29,10 @@ class garis:
         x = self.kalkulasi_garistemu(y,r)
         print(x)
         self.KoorCerminBawah = (self.cermin.Center[0]+x,self.bayangan.cooratas[1])
-        DrawC.dda(self.bayangan.cooratas,self.KoorCerminBawah,(255,255,0))
+        DrawC.dda(self.bayangan.cooratas,self.KoorCerminBawah,self.warna)
         self.gif()
 
     def gif(self):
-        DrawC.dda(self.KoorCerminAtas,self.bayangan.cooratas,(255,255,0))
-        DrawC.dda(self.benda.kooratas,self.KoorCerminBawah,(255,255,0))
+        DrawC.dda(self.KoorCerminAtas,self.bayangan.cooratas,self.warna)
+        DrawC.dda(self.benda.kooratas,self.KoorCerminBawah,self.warna)
     
