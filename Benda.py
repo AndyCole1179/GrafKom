ecae22093e96044d02de0705f25d825bf170b3f7
AndyCole1179 <134572850+AndyCole1179@users.noreply.github.com>
2024-02-10 17:11:03 +0800

    Update Benda.py

--- Benda.py
+++ Benda.py
@@ -8,11 +8,13 @@ class benda:
         self.warna = (255,0,0)
         self.jarak = None
         self.kooratas = None
+        self.koor = None
 
     def gambar(self,jarak,tinggi):
         self.tinggi = tinggi
         self.jarak = jarak
         koor_bawah = (self.cermin.perimeter[0]-jarak,self.cermin.perimeter[1])
+        self.koor = koor_bawah
         koor_atas =  (koor_bawah[0],koor_bawah[1]-tinggi)
         self.kooratas = koor_atas
         DrawC.dda(koor_bawah,koor_atas,self.warna)
