47007d98127caf9f8a3034cc6dcd60f40df1730d
AndyCole1179 <134572850+AndyCole1179@users.noreply.github.com>
2024-02-10 17:12:44 +0800

    Update garis_cahaya.py

--- garis_cahaya.py
+++ garis_cahaya.py
@@ -11,28 +11,51 @@ class garis:
         self.warna = (255,255,0)
 
     def kalkulasi_garistemu(self,y,r):
-        x = math.ceil(math.sqrt((r**2)-(y**2)))
+        x = (math.ceil(math.sqrt((r**2)-(y**2))))
 
         return x
-    def gbc(self): #garis benda cermin
+    
+    def gbc(self): #cermin = lingkaran
         y = self.benda.tinggi
         r = self.cermin.Radius
         x = self.kalkulasi_garistemu(y,r)
-        print(x)
         self.KoorCerminAtas = (self.cermin.Center[0]+x,self.benda.kooratas[1])
         DrawC.dda(self.benda.kooratas,self.KoorCerminAtas,self.warna)
         self.gic()
     
-    def gic(self):
+    def gbcgaris(self):#cermin = garis
+        y = self.benda.tinggi
+        r = self.cermin.Radius
+        self.KoorCerminAtas = (self.cermin.Center[0]+r,self.benda.kooratas[1])
+        DrawC.dda(self.benda.kooratas,self.KoorCerminAtas,self.warna)
+        self.gicgaris()
+
+    def gic(self):# Cermin = lingkaran
         y = self.bayangan.tinggi
         r = self.cermin.Radius
         x = self.kalkulasi_garistemu(y,r)
-        print(x)
         self.KoorCerminBawah = (self.cermin.Center[0]+x,self.bayangan.cooratas[1])
+        self.gif()
+
+    def gicgaris(self): # Cermin = garis
+        y = self.bayangan.tinggi
+        r = self.cermin.Radius
+        self.KoorCerminBawah = (self.cermin.Center[0]+r,self.bayangan.cooratas[1])
         DrawC.dda(self.bayangan.cooratas,self.KoorCerminBawah,self.warna)
         self.gif()
 
     def gif(self):
-        DrawC.dda(self.KoorCerminAtas,self.bayangan.cooratas,self.warna)
-        DrawC.dda(self.benda.kooratas,self.KoorCerminBawah,self.warna)
+
+        if self.benda.koor[0] > self.cermin.focus[0]  and self.benda.koor[0]< self.cermin.perimeter[0]:#Ruang 4
+            DrawC.dda(self.bayangan.cooratas,self.KoorCerminAtas,self.warna)
+            DrawC.dda(self.KoorCerminBawah,self.benda.kooratas,self.warna)
+
+        elif self.benda.koor[0] > self.cermin.focus[0]  and self.benda.koor[0] > self.cermin.perimeter[0]:#Ruang 1
+            DrawC.dda(self.KoorCerminAtas,self.bayangan.cooratas,self.warna)
+            DrawC.dda(self.KoorCerminBawah,self.benda.kooratas,self.warna)
+
+        else : # Ruang 2 dan 3
+            DrawC.dda(self.KoorCerminAtas,self.bayangan.cooratas,self.warna)
+            DrawC.dda(self.benda.kooratas,self.KoorCerminBawah,self.warna)
+
     
