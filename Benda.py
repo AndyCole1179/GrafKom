c8cd1d3c43b1e6545bedca13c343bfd4e3e58cdc
AndyCole1179 <134572850+AndyCole1179@users.noreply.github.com>
2024-02-16 01:08:57 +0800

    Update Benda.py

--- Benda.py
+++ Benda.py
@@ -10,12 +10,33 @@ class benda:
         self.kooratas = None
         self.koor = None
 
-    def gambar(self,jarak,tinggi):
+    def gambar(self,jarak,tinggi,lebar):
+        self.lebar = lebar
         self.tinggi = tinggi
         self.jarak = jarak
-        koor_bawah = (self.cermin.perimeter[0]-jarak,self.cermin.perimeter[1])
-        self.koor = koor_bawah
-        koor_atas =  (koor_bawah[0],koor_bawah[1]-tinggi)
-        self.kooratas = koor_atas
-        DrawC.dda(koor_bawah,koor_atas,self.warna)
-    
+        if jarak < 0 :
+            koor_bawah = (self.cermin.perimeter[0]-jarak,self.cermin.perimeter[1])
+            self.koor = koor_bawah
+            koor_atas =  (koor_bawah[0],koor_bawah[1]-tinggi)
+            self.kooratas = koor_atas
+            TKiriAtas = (int(koor_atas[0]+(lebar/2)),koor_atas[1])
+            TKiriBawah = (int(koor_bawah[0]+(lebar/2)),koor_bawah[1])
+
+        else :
+            koor_bawah = (self.cermin.perimeter[0]-jarak,self.cermin.perimeter[1])
+            self.koor = koor_bawah
+            koor_atas =  (koor_bawah[0],koor_bawah[1]-tinggi)
+            self.kooratas = koor_atas
+            TKiriAtas = (koor_atas[0]-(lebar/2),koor_atas[1])
+            TKiriBawah = (koor_bawah[0]-(lebar/2),koor_bawah[1])
+
+
+        DrawC.dda(TKiriAtas,koor_atas,self.warna)
+        DrawC.dda(TKiriBawah,TKiriAtas,self.warna)
+        DrawC.dda(koor_atas,koor_bawah,self.warna)
+        DrawC.dda(TKiriBawah,koor_bawah,self.warna)
+        
+        #DrawC.dda(koor_bawah,koor_atas,self.warna)
+
+
+
