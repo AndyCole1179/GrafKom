5d5faa1dc28924aae651bc334485851f2f7bbd77
AndyCole1179 <134572850+AndyCole1179@users.noreply.github.com>
2024-02-22 12:33:54 +0800

    Update Draw.py

--- Draw.py
+++ Draw.py
@@ -10,32 +10,33 @@ class DrawC:
         glVertex2f(math.ceil(x),math.ceil(y))
         glEnd()
 
-    def dda(Coor1,Coor2,rgb):
-        x1 = Coor1[0]
-        y1 = Coor1[1]
-        x2 = Coor2[0]
-        y2 = Coor2[1]
-        dx = x2-x1
-        dy = y2-y1
-        if abs(dx) > abs(dy):
-            if(x2 > x1):
-                y = y1
-                for x in range(int(x1), int(x2) + 1):
-                    y = y + dy/abs(dx)
-                    DrawC.gambar(x,y,rgb)
-            else:
-                y = y1
-                for x in range(x2, x1 - 1):
-                    y = y + dy/abs(dx)
-                    DrawC.gambar(x,y,rgb)     
+    def dda(Coor1, Coor2, rgb):
+        x1, y1 = Coor1
+        x2, y2 = Coor2
+        dx = x2 - x1
+        dy = y2 - y1
+        steps = max(abs(dx), abs(dy))
+
+        if dx == 0: 
+            x_increment = 0
         else:
-            if(y2 > y1):
-                x = x1
-                for y in range(y1, y2 + 1):
-                    x = x + dx/abs(dy)
-                    DrawC.gambar(x,y,rgb)
-            else:
-                x = x1
-                for y in range(y2, y1 - 1):
-                    x = x + dx/abs(dy)
-                    DrawC.gambar(x,y,rgb)
+            x_increment = dx / steps
+
+        if dy == 0: 
+            y_increment = 0
+        else:
+            y_increment = dy / steps
+
+        x = x1
+        y = y1
+        x_rounded = round(x)
+        y_rounded = round(y)
+        DrawC.gambar(x_rounded, y_rounded, rgb)
+
+        for _ in range(int(steps)):
+            x += x_increment
+            y += y_increment
+            x_rounded = round(x)
+            y_rounded = round(y)
+
+            DrawC.gambar(x_rounded, y_rounded, rgb)
